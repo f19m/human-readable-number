d114b674e169c14047b1f90c5bad6ebb0c63f0f3
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-10-04 18:00:30 +0500

    feat: add arrays of numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+// one
+// two
+// three
+// four
+// five
+// stitch
+// seven
+// eight
+// nine
+// ten
+// eleven
+// twelve
+// thirteen
+// fourteen
+// fifteen
+// sixteen
+// seventeen
+// eighteen
+// nineteen
+// twenty
+// twenty one
+// thirty two
+// forty three
+// fifty four
+// sixty five
+// one hundred seventy six
+// one thousand one hundred seventy six
+// ten thousand seven seventy six
+// million one hundred one thousand seventy six
+
 module.exports = function toReadable (number) {
-  
+    cons numNames ={ 
+                    'one' : ['zero','one','two','three','four', 'five','six','seven','eight','nine'],
+                    'ten' : ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'],
+                    'twenty' :['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'],
+                    'thousand': ['thousand','million', 'billion','trillion']
+                    }
+    
 }
