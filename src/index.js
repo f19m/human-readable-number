1349413e810ff6658c065d3876e0f3e25eec2a18
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-10-05 16:14:53 +0500

    feat: task resolved

--- src/index.js
+++ src/index.js
@@ -28,12 +28,53 @@
 // ten thousand seven seventy six
 // million one hundred one thousand seventy six
 
+const numNames = { 
+    '0' : ['zero','one','two','three','four', 'five','six','seven','eight','nine'],
+    '10' : ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'],
+    '20' :['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'],
+    '100': ['hundred','thousand','million', 'billion','trillion']
+    };
+
+
 module.exports = function toReadable (number) {
-    cons numNames ={ 
-                    'one' : ['zero','one','two','three','four', 'five','six','seven','eight','nine'],
-                    'ten' : ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'],
-                    'twenty' :['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'],
-                    'thousand': ['thousand','million', 'billion','trillion']
-                    }
+    
+    let n = 12;
+    
+    if (number == 0){
+        return 'zero';
+    }
+
+    let mod = 0;
+    while (mod== 0 && n > 0){
+        if (n > 3){
+            n = n - 3;
+        }else{
+            n = n - 1
+        }
+        mod = Math.floor(number / (10 ** n));
+    }
+    let ost = number - mod * (10**n)
+
+    let res;
+
+    if (n==0){
+        return numNames['0'][mod]
+    } else if (n == 1 && mod == 1) {
+        return numNames['10'][ost]
+    }else if (n == 1 && mod > 1) {
+        res = numNames['20'][mod-2]
+        if (ost > 0){ 
+            return res + ' ' + toReadable(ost)
+        }
+        return res
+    }else if (n > 1) {
+        res = toReadable(mod) + ' ' + numNames['100'][Math.floor(n/3)]
+        if (ost > 0){ 
+            return res + ' ' + toReadable(ost)
+        }
+        return res
+    }
+       
+    
     
 }
